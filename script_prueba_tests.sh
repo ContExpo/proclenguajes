9a29bea970dfd10539a1bea529711a934c55ea32
Alessio Esposito Inchiostro <20034674@studenti.uniupo.it>
2022-04-04 14:14:13 +0200

    Added correct script for our folder structure

--- script_prueba_tests.sh
+++ script_prueba_tests.sh
@@ -2,10 +2,10 @@
 
 #Este es un script para poder revisar los errores de todos los programas de prueba de una tajada
 
-cd ../../adac/adac
+cd ./adac_pruebas
 
 for nom_prueba in *.adac; do
 	echo "Probando ${nom_prueba}:"
-	java -jar ../../practica_1/adac_lexico/dist/adac.jar < "$nom_prueba" | grep Error
+	java -jar ../dist/adac.jar < "$nom_prueba" | grep Error
 	echo "Prueba concluida."
 done
